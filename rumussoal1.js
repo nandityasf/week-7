3461d11d5b9a4bcede7f1d0278cba7a5ca8f773d
nandityasf <you@example.com>
2023-09-22 21:04:51 +0700

    rumus untuk soal 1

---
+++ rumussoal1.js
@@ -0,0 +1,19 @@
+// persegi= keliling:4xs, luas:sxs 
+// s=8
+exports.addition = function(numberOne, numberTwo, numberThree, numberFour){
+    return numberOne + numberTwo + numberThree + numberFour
+}
+
+exports.multiply = function(numberOne, numberTwo){
+    return numberOne * numberTwo
+}
+
+// p.panjang= keliling:semua sisi ditambah, luas:pxl
+// p=10 l=7
+exports.addition = function(numberOne, numberTwo, numberThree, numberFour){
+    return numberOne + numberTwo + numberThree + numberFour
+}
+
+exports.multiply = function(numberOne, numberTwo){
+    return numberOne * numberTwo
+}
